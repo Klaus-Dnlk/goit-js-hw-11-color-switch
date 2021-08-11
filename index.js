ad7e3b7cc00d7766cd1c4e585bbbad35a5b2901d
Klaus-Dnlk <79231530+Klaus-Dnlk@users.noreply.github.com>
2021-08-11 17:31:40 +0300

    убрал лишнее

--- index.js
+++ index.js
@@ -6,30 +6,27 @@ const colors = [
     '#009688',
     '#795548',
   ];
-  const refs = {
+
+const refs = {
     start: document.querySelector('[data-action="start"]'),
     stop: document.querySelector('[data-action="stop"]'),
     body: document.querySelector('body')
 }
 
-//   функция для генерации цветов
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 let id = null;
 
-function colorChanger () {
-  const currentColor = randomIntegerFromInterval(0, colors.length);
-  refs.body.style.backgroundColor = colors[currentColor];
-  console.log(currentColor);
+function changeColor () {
+  const currentColorIndex = randomIntegerFromInterval(0, colors.length);
+  refs.body.style.backgroundColor = colors[currentColorIndex];
 }
 
-
 function startColorChange () {
-  id = setInterval(colorChanger, 1000);
+  id = setInterval(changeColor, 1000);
   refs.start.disabled = true;
-  console.log();
 }
 
 function stopColorChange() {
